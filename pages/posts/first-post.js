7d81e52971c5e59d01b0c1755b845b9df5abc204
pauline-wanjiku <paulinendungushiko96@gmail.com>
2023-01-20 14:24:10 +0300

    use of Link for routing in next.js

---
+++ pages/posts/first-post.js
@@ -0,0 +1,11 @@
+import Link from "next/link";
+
+function FirstPost() {
+  return (
+    <>
+          <h1>First Post</h1>
+          <Link href="/">Back to home</Link>
+    </>
+  );
+}
+export default FirstPost;
