e7d03d5aff9deb80104b117d92cccd67a423ca00
pauline-wanjiku <paulinendungushiko96@gmail.com>
2023-01-23 15:17:05 +0300

    Images and Script components

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,10 +1,29 @@
+import Head from "next/head";
+import Image from "next/image";
 import Link from "next/link";
+import Script from "next/script";
 
 function FirstPost() {
   return (
     <>
-          <h1>First Post</h1>
-          <Link href="/">Back to home</Link>
+      <Head>
+        <title>First Post</title>
+      </Head>
+      <h1>First Post</h1>
+      <Script
+        src="https://connect.facebook.net/en_US/sdk.js"
+        strategy="lazyOnload"
+        onLoad={() =>
+          console.log(`script loaded correctly, window.FB has been populated`)
+        }
+      />
+
+      <Link href="/">Back to home</Link>
+      <Image
+        src="/images/Pauline's.jpg"
+        height={144}
+        width={144}
+        alt={"profile picture"}></Image>
     </>
   );
 }
